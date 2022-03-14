710819210c6f33510b6b64293238420b8b344771
Geraseva Elizaveta <geraseva@srv.vsb.fbb.msu.ru>
2022-03-14 11:09:40 +0300

    initial

---
+++ data_preprocessing/convert_pdb2npy.py
@@ -0,0 +1,45 @@
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+from Bio.PDB import *
+
+ele2num = {"C": 0, "H": 1, "O": 2, "N": 3, "S": 4, "SE": 5, "P": 6, "F": 7, "CL": 8, "BR": 9, "I": 10}
+
+
+def load_structure_np(fname, center):
+    """Loads a .ply mesh to return a point cloud and connectivity."""
+    # Load the data
+    parser = PDBParser()
+    structure = parser.get_structure("structure", fname)
+    atoms = structure.get_atoms()
+
+    coords = []
+    types = []
+    res=[]
+    for atom in atoms:
+        if atom.element in ele2num.keys():
+            coords.append(atom.get_coord())
+            types.append(ele2num[atom.element])
+            res.append(atom.get_parent().get_resname())
+        else: 
+            print('Unknown atom',atom.element, 'in', str(fname).split('/')[-1])
+
+    coords = np.stack(coords)
+    types_array = np.zeros((len(types), len(ele2num)))
+    for i, t in enumerate(types):
+        types_array[i, t] = 1.0
+    res=np.array(res)
+
+    # Normalize the coordinates, as specified by the user:
+    if center:
+        coords = coords - np.mean(coords, axis=0, keepdims=True)
+        
+    return {"xyz": coords, "types": types_array, "resnames": res}
+
+
+def convert_pdbs(pdb_dir, npy_dir):
+    print("Converting PDBs")
+    for p in tqdm(pdb_dir.glob("*.pdb")):
+        protein = load_structure_np(p, center=False)
+        np.save(npy_dir / (p.stem + "_atomxyz.npy"), protein["xyz"])
+        np.save(npy_dir / (p.stem + "_atomtypes.npy"), protein["types"])
---
+++ masif_npi/benchmark.sh
@@ -0,0 +1,13 @@
+
+
+masif_root=$(git rev-parse --show-toplevel)
+
+export PYTHONPATH=${PYTHONPATH}:${masif_root}
+
+#python $masif_root/data_preprocessing/download_pdb.py --pdb_list npidb.list --save_res True 
+
+#export PYTORCH_NO_CUDA_MEMORY_CACHING=1
+#export CUDA_LAUNCH_BLOCKING=1 
+
+python -W ignore::FutureWarning -u ${masif_root}/masif_npi/training.py --device cuda:0 --experiment_name try_npi_3layers_9rad --batch_size 64 --embedding_layer dMaSIF --npi True --single_protein True --random_rotation True --radius 9.0 --n_layers 3
+
