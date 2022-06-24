8534981018c74f568c0fc2a469574ecf0e7bd0d1
geraseva <62103345+geraseva@users.noreply.github.com>
2022-06-24 23:49:10 +0300

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,36 @@ export PYTHONPATH=${PYTHONPATH}:${masif_root}
 
 #python $masif_root/data_preprocessing/download_pdb.py --pdb_list npidb.list --save_res True 
 
-#export PYTORCH_NO_CUDA_MEMORY_CACHING=1
-#export CUDA_LAUNCH_BLOCKING=1 
+python -W ignore::FutureWarning -u ${masif_root}/training.py --device cuda:0 \
+ --experiment_name orig --batch_size 64 --embedding_layer dMaSIF --site True \
+ --single_protein True --random_rotation True --radius 9.0 --n_layers 3 --loss BCELoss \
+ --feature_generation AtomNet --dataset ProteinPairsSurfaces # 0.8559492105941394
+
+python -W ignore::FutureWarning -u ${masif_root}/training.py --device cuda:0 \
+ --experiment_name orig_v --batch_size 64 --embedding_layer dMaSIF --site True \
+ --single_protein True --random_rotation True --radius 9.0 --n_layers 3 --loss BCELoss \
+ --feature_generation AtomNet_V --dataset ProteinPairsSurfaces # 0.8380108538715837
+
+python -W ignore::UserWarning -u ${masif_root}/training.py --device cuda:0 \
+ --experiment_name orig_dgcnn --batch_size 64 --embedding_layer DGCNN --site True \
+ --single_protein True --random_rotation True --n_layers 3 --loss BCELoss \
+ --feature_generation AtomNet_MP --dataset ProteinPairsSurfaces # 0.7777782407068814
+
+
+python -W ignore::UserWarning -u ${masif_root}/training.py --device cuda:0 \
+ --experiment_name binary_dgcnn --batch_size 64 --embedding_layer DGCNN --site True \
+ --single_protein True --random_rotation True --n_layers 3 --loss BCELoss \
+ --feature_generation AtomNet --dataset NpiDataset # 0.7716083364077496
+
+python -W ignore::UserWarning -u ${masif_root}/training.py --device cuda:0 \
+ --experiment_name binary_atomnet --batch_size 64 --embedding_layer dMaSIF --site True \
+ --single_protein True --random_rotation True --radius 9.0 --n_layers 3 --loss BCELoss \
+ --feature_generation AtomNet --dataset NpiDataset # 0.9074962983309375
+
+ python -W ignore::UserWarning -u ${masif_root}/training.py --device cuda:0 \
+ --experiment_name binary_atomnetv --batch_size 64 --embedding_layer dMaSIF --site True \
+ --single_protein True --random_rotation True --radius 9.0 --n_layers 3 --loss BCELoss \
+ --feature_generation AtomNet_V --dataset NpiDataset # 0.9251051725638855
 
 
 python -W ignore::FutureWarning -u ${masif_root}/training.py --device cuda:0 \
