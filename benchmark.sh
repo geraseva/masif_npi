909242bf292aee28b5d536f33dcb33643c46cc77
geraseva <liza.geraseva@bk.ru>
2023-04-24 14:11:20 +0300

    upd

--- benchmark.sh
+++ benchmark.sh
@@ -9,124 +9,132 @@ export PYTHONPATH=${PYTHONPATH}:${masif_root}
 
 python  ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig --site \
- --feature_generation AtomNet --na protein
+ --feature_generation AtomNet --na protein >> logs/log_orig.txt 
 
 python  ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_v --site \
- --feature_generation AtomNet_V --na protein
+ --feature_generation AtomNet_V --na protein >> logs/log_orig.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_mp --site \
- --feature_generation AtomNet_MP --na protein
+ --feature_generation AtomNet_MP --na protein >> logs/log_orig.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_v_mp --site \
- --feature_generation AtomNet_V_MP --na protein
+ --feature_generation AtomNet_V_MP --na protein >> logs/log_orig.txt
 
 # binary prediction of dna-protein interaction site
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_dna_atomnet --site \
- --feature_generation AtomNet --na DNA
+ --feature_generation AtomNet --na DNA >> logs/log_dna_site.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_dna_atomnet_v --site \
- --feature_generation AtomNet_V --na DNA
+ --feature_generation AtomNet_V --na DNA >> logs/log_dna_site.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_dna_atomnet_mp --site \
- --feature_generation AtomNet_MP --na DNA 
+ --feature_generation AtomNet_MP --na DNA >> logs/log_dna_site.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_dna_atomnet_v_mp --site \
- --feature_generation AtomNet_V_MP --na DNA 
+ --feature_generation AtomNet_V_MP --na DNA >> logs/log_dna_site.txt
 
 # binary prediction of rna-protein interaction site
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_rna_atomnet --site \
- --na RNA --feature_generation AtomNet 
+ --na RNA --feature_generation AtomNet >> logs/log_rna_site.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_rna_atomnet_v --site \
- --na RNA --feature_generation AtomNet_V 
+ --na RNA --feature_generation AtomNet_V >> logs/log_rna_site.txt 
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_rna_atomnet_mp --site \
- --na RNA --feature_generation AtomNet_MP 
+ --na RNA --feature_generation AtomNet_MP >> logs/log_rna_site.txt 
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name binary_rna_atomnet_v_mp --site \
- --na RNA --feature_generation AtomNet_V_MP 
+ --na RNA --feature_generation AtomNet_V_MP >> logs/log_rna_site.txt 
 
 # prediction of nucleotides in dna-protein interaction site
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_dna_atomnet --npi \
- --feature_generation AtomNet  --na DNA
+ --feature_generation AtomNet  --na DNA >> logs/log_dna_npi.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_dna_atomnet_v --npi \
- --feature_generation AtomNet_V  --na DNA
+ --feature_generation AtomNet_V  --na DNA >> logs/log_dna_npi.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_dna_atomnet_mp --npi \
- --feature_generation AtomNet_MP  --na DNA
+ --feature_generation AtomNet_MP  --na DNA >> logs/log_dna_npi.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_dna_atomnet_v_mp --npi \
- --feature_generation AtomNet_V_MP  --na DNA
+ --feature_generation AtomNet_V_MP  --na DNA >> logs/log_dna_npi.txt
 
 # prediction of nucleotides in rna-protein interaction site
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_rna_atomnet --npi \
- --na RNA --feature_generation AtomNet 
+ --na RNA --feature_generation AtomNet >> logs/log_rna_npi.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_rna_atomnet_v --npi \
- --na RNA --feature_generation AtomNet_V 
+ --na RNA --feature_generation AtomNet_V >> logs/log_rna_npi.txt 
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_rna_atomnet_mp --npi \
- --na RNA --feature_generation AtomNet_MP 
+ --na RNA --feature_generation AtomNet_MP >> logs/log_rna_npi.txt 
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_rna_atomnet_v_mp --npi \
- --na RNA --feature_generation AtomNet_V_MP 
+ --na RNA --feature_generation AtomNet_V_MP >> logs/log_rna_npi.txt 
 
  # prediction of interactions on PPI dataset
 
 python  ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_search_atomnet --search \
- --feature_generation AtomNet --na protein
+ --feature_generation AtomNet --na protein \
+ --radius 12 --n_layers 1  >> logs/log_orig_search.txt
 
 python  ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_search_atomnet_v --search \
- --feature_generation AtomNet_V --na protein
+ --feature_generation AtomNet_V --na protein \
+ --radius 12 --n_layers 1  >> logs/log_orig_search.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_search_atomnet_mp --search \
- --feature_generation AtomNet_MP --na protein
+ --feature_generation AtomNet_MP --na protein \
+ --radius 12 --n_layers 1  >> logs/log_orig_search.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name orig_search_atomnet_v_mp --search \
- --feature_generation AtomNet_V_MP --na protein
+ --feature_generation AtomNet_V_MP --na protein \
+ --radius 12 --n_layers 1  >> logs/log_orig_search.txt
 
  # prediction of interactions on NPI dataset
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_search_atomnet --search \
- --feature_generation AtomNet --na NA
+ --feature_generation AtomNet --na NA \
+ --radius 12 --n_layers 1  >> logs/log_na_search.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_search_atomnet_v --search \
- --feature_generation AtomNet_V --na NA
+ --feature_generation AtomNet_V --na NA \
+ --radius 12 --n_layers 1  >> logs/log_na_search.txt
 
 python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_search_atomnet_mp --search \
- --feature_generation AtomNet_MP --na NA 
+ --feature_generation AtomNet_MP --na NA \
+ --radius 12 --n_layers 1  >> logs/log_na_search.txt
 
  python ${masif_root}/training.py --device cuda:0 \
  --experiment_name npi_search_atomnet_v_mp --search \
- --feature_generation AtomNet_V_MP --na NA 
+ --feature_generation AtomNet_V_MP --na NA \
+ --radius 12 --n_layers 1  >> logs/log_na_search.txt
